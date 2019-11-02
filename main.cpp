036f99161d764844c3ac0de0a7c4bef1efd9d938
yunjin-cho <cho328@purdue.edu>
2019-11-02 03:13:07 -0400

    Created structure of main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 // Yunjin Cho
 
-#include <iostream>
+#include "interpreter/State.h"
+#include "interpreter/Instructions.h"
 
 int main(int argc, char **argv) {
-    std::cout << "Hello\n";
+    State s(argv[1]);
+    for (uint8_t ins; (ins = s.nextInstruction()) != Halt; )
+        instructions[ins](s);
 }
