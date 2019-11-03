7b683db492982149e920423c1618a10278ad11d0
yunjin-cho <cho328@purdue.edu>
2019-11-03 01:39:20 -0500

    Made instruction handlers static

--- src/Instructions.cpp
+++ src/Instructions.cpp
@@ -42,77 +42,77 @@ struct eq {
 };
 
 template <class comparator>
-void cmp(State& s) {
+static void cmp(State& s) {
     Stack<Data>& stack = s.getStack();
     Data right = stack.pop(), left = stack.pop();
     stack.push({Type::Int, comparator()(left, right)});
 }
 
 template <typename IntType, Type type>
-void push(State& s) {
+static void push(State& s) {
     IntType i = *reinterpret_cast<const IntType*>(s.text() + s.pc);
     s.pc += sizeof(IntType);
     s.stack.push({type, static_cast<uint32_t>(i)});
 }
 
-void pushf(State& s) {
+static void pushf(State& s) {
     float f = *reinterpret_cast<const float*>(s.text() + s.pc);
     s.pc += sizeof(float);
     s.stack.push({Type::Float, *reinterpret_cast<uint32_t*>(&f)});
 }
 
 template <Type type>
-void pushv(State& s) {
+static void pushv(State& s) {
     uint32_t d = s.stack.at(s.fpstack.top() + s.stack.top().getData() + 1).getData();
     s.stack.pop();
     s.stack.push({type, d});
 }
 
 template <typename operation>
-void arithmetic(State& s) {
+static void arithmetic(State& s) {
     Stack<Data>& stack = s.getStack();
     Data right = stack.pop(), left = stack.pop();
     stack.push(operation()(left, right));
 }
 
 template <typename type>
-void print(State& s) {
+static void print(State& s) {
     uint32_t i = s.getStack().top().getData();
     s.os() << *reinterpret_cast<type*>(&i);
 }
 
-void jmp(State& s) {
+static void jmp(State& s) {
     s.pc = s.stack.pop().getData();
 }
 
-void jmpc(State& s) {
+static void jmpc(State& s) {
     if (s.stack.top(1).getData())
         s.pc = s.stack.top().getData();
     s.stack.pop(2);
 }
 
-void call(State& s) {
+static void call(State& s) {
     s.fpstack.push(s.sp() - s.stack.top().getData() - 1);
     s.stack.pop();
     s.pc = s.stack.pop().getData();
 }
 
-void ret(State& s) {
+static void ret(State& s) {
     s.stack.resize(s.fpstack.pop());
     s.pc = s.stack.pop().getData();
 }
 
-void popm(State& s) {
+static void popm(State& s) {
     uint32_t num = s.stack.pop().getData();
     s.stack.pop(num);
 }
 
-void popv(State& s) {
+static void popv(State& s) {
     s.stack.at(s.fpstack.top() + s.stack.top().getData() + 1) = s.stack.top(1);
     s.stack.pop(2);
 }
 
-void popa(State& s) {
+static void popa(State& s) {
     uint32_t data = s.stack.pop();
     std::stack<Data> stack;
     for (int i = 0; i < data; i++)
